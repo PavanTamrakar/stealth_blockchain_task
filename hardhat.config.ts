558aaef3eec2bfb5b2dbda0260f4ce025a1c6790
Pavan Tamrakar <thepavantamrakar@gmail.com>
2024-02-04 19:17:35 +0530

    test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,11 +8,8 @@ import "solidity-coverage"
 import "hardhat-deploy"
 import { HardhatUserConfig } from "hardhat/config"
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const SEPOLIA_RPC_URL =
-  process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -26,10 +23,9 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true
     },
     sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
-    },
+      url: BASE_SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    }
   },
   solidity: {
     version: "0.8.18",
@@ -40,15 +36,12 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
+
   gasReporter: {
     enabled: true,
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    // coinmarketcap: COINMARKETCAP_API_KEY,
   },
   namedAccounts: {
     deployer: {
