dc365d7c7b195be316c7979ae199f83364081ad8
Pavan Tamrakar <thepavantamrakar@gmail.com>
2024-02-04 19:21:30 +0530

    test

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract baseToken is ERC20Votes {
+contract GovernanceToken is ERC20Votes {
   uint256 public s_maxSupply = 1000000000000000000000000;
 
-  constructor() ERC20("baseToken", "GT") ERC20Permit("baseToken") {
+  constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
     _mint(msg.sender, s_maxSupply);
   }
 
